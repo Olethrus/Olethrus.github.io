2770438d03a79eff2c57ec4ab410bce298d94c4f
William Sylvester <72401451+Olethrus@users.noreply.github.com>
2023-03-15 20:06:50 -0500

    Update nav.js

--- js/nav.js
+++ js/nav.js
@@ -1,4 +1,19 @@
-function showDropdown() {
+function showDropdown() 
+{
   var dropdown = document.getElementById("navDropdown");
   dropdown.style.display = "block";
 }
+function hideDropdown() 
+{
+  var dropdown = document.getElementById("myDropdown");
+  dropdown.style.display = "none";
+}
+
+window.onclick = function(event) 
+{
+  if (!event.target.matches('#myDropdown') && !event.target.matches('#showDropdownButton')) 
+  {
+    hideDropdown();
+  }
+}
+
