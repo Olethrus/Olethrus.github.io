b6e32818a359208acd442d08b691b2e398d34697
William Sylvester <72401451+Olethrus@users.noreply.github.com>
2023-03-15 20:05:10 -0500

    Update nav.js

--- js/nav.js
+++ js/nav.js
@@ -1,15 +1,4 @@
-$(document).ready(function() {
-  $('nav li').click(function(e) {
-    if ($(this).hasClass('active')) {
-      $(this).removeClass('active');
-    } else {
-      $('nav li').removeClass('active');
-      $(this).addClass('active');
-    }
-    e.stopPropagation();
-  });
-
-  $(document).click(function() {
-    $('nav li').removeClass('active');
-  });
-});
+function showDropdown() {
+  var dropdown = document.getElementById("navDropdown");
+  dropdown.style.display = "block";
+}
